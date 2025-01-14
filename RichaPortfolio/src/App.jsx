234bdc4d3666d6d633b55f9bdc4e99fd8d948dca
RichaGeeks <richasuryawanshi1505@gmail.com>
2025-01-14 14:23:22 +0530

    my first commit on portfolio

--- RichaPortfolio/src/App.jsx
+++ RichaPortfolio/src/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import "./style.css"
-import {Canvas} from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei" ;
-const App = () => {
+import "./style.css";
+import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import Cyl from "./cyl";
 
+const App = () => {
   return (
-    <Canvas>
-      <OrbitControls/>
-      <ambientLight />
-      <mesh>
-        <cylinderGeometry/>
-        <boxGeometry/>
-        <meshStandardMaterial/>
-      </mesh>
+    <Canvas camera={{ fov: 30 }} >
+      <OrbitControls />
+      <ambientLight intensity={8} />
+      <directionalLight  position={[8, 0, 0]} intensity={0.03} />
+      <Cyl />
+      <EffectComposer>
+        <ToneMapping adaptive={false} exposure={3.5} />
+        <Bloom
+          mipmapBlur
+          intensity={5}
+          luminanceThreshold={0.8}
+          luminanceSmoothing={0.7}
+        />
+      </EffectComposer>
     </Canvas>
-  )
+  );
 };
 
 export default App;
